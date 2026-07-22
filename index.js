a4e945d6d6d58af9e5dd07f0a543d667271af75a
Kantamneni Sai Vamsi <saivamsikantamneni@gmail.com>
2026-07-22 23:51:26 +0530

    Stage 3: create with validation

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express() ;
 const PORT = 3000 ;
 
-const tasks = [
+let tasks = [
     {id : 1 , title:"do flyrank assignment" , done: true},
     {id: 2 , title:"do leetcode problems" , done: false},
     {id: 3 , title:"do codeforces problems", done: false}
@@ -19,6 +19,7 @@ const status = { "status": "ok" } ;
 
 app.use(express.json()) ;
 
+// --------------------------------------------------
 // STAGE - 1
 app.get('/',(req,res)=>{
     res.json(descirbe) ;
@@ -30,6 +31,7 @@ app.get('/health' , (req,res)=>{
     //res.status(200).end(JSON.stringify(data)) ;
 });
 
+// --------------------------------------------------
 // STAGE - 2
 app.get('/tasks',(req,res)=>{
     res.json(tasks);
@@ -53,9 +55,36 @@ app.get('/tasks/:id',(req,res)=>{
     }
 });
 
-app.get('/tasks/:',)
+// --------------------------------------------------
+// STAGE - 3
+app.post('/tasks',(req,res)=>{
+    let next_id = tasks.length +1 ;
+
+    if(Object.keys(req.body).length === 0){
+        res.status(400).json({error:"Body of POST request cannot be empty"}) ;
+    }
+    else if(!req.body.title){
+        res.status(400).json({error:"Title not found in request"}) ;
+    }
+    else{
+        // .trim() method removes the extra spaces before and after the string 
+        if(req.body.title.trim() === ""){
+            res.status(400).json({error:"Title cannot be empty"}) ;
+            res.end() ;
+        }else{
+            let task_title = req.body.title ; 
+            tasks = [
+                ...tasks,
+                {id:next_id , title: task_title , done:false}
+            ];
+            res.status(201).json(tasks[next_id-1]);
+        }
+    }
+    //console.log(typeof(req.body));
+});
 
 
+// --------------------------------------------------
 // STAGE - 0
 app.listen(PORT,()=>{
     console.log(`App is listening to port: ${PORT}`) ;
