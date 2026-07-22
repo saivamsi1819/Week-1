2e8e2d72074af9afeff84adef0a80e55f7da331a
Kantamneni Sai Vamsi <saivamsikantamneni@gmail.com>
2026-07-22 21:48:15 +0530

    Stage 1: root and health endpoints

--- index.js
+++ index.js
@@ -3,10 +3,26 @@ import express from 'express';
 const app = express() ;
 const PORT = 3000 ;
 
+const descirbe = {
+         "name": "Task API", 
+         "version": "1.0", 
+         "endpoints": ["/tasks"] 
+};
+
+const status = { "status": "ok" } ;
+
+app.use(express.json()) ;
+
 app.get('/',(req,res)=>{
-    res.status(200).end("Hello World") ;
+    
+    res.json(descirbe) ;
+    // res.status(200).end(JSON.stringify(data)) ;
 });
 
+app.get('/health' , (req,res)=>{
+    res.json(status);
+    //res.status(200).end(JSON.stringify(data)) ;
+});
 
 app.listen(PORT,()=>{
     console.log(`App is listening to port: ${PORT}`) ;
